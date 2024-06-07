e6e6ef835d41b8dfe365fe057ad32197253b07f6
Peywan Sabir <peywan1999@icloud.com>
2024-06-07 21:55:23 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { useState } from 'react'
+
+
+function App() {
+const [todos, setTodos] = useState([])
+  return (
+    <>
+        <h1>Todo app by Peywan</h1>
+    </>
+  )
+}
+
+export default App
